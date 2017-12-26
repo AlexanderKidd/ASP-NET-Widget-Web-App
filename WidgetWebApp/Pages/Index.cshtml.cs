fa29c83428929cc27c89bf64dd8e72b647e834ea
Alexander Kidd <alexanderkidd5@outlook.com>
2017-12-25 22:11:07 -0500

    Add project files.

---
+++ WidgetWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+/* This is meant to make model-binding easy: 
+ * https://docs.microsoft.com/en-us/aspnet/core/mvc/models/model-binding
+ * Basically, dump the HTTP data automatically into a model instead of 
+ * extracting w/ controller.
+ * 
+ * Razor pages also make it easier to drop-in tag & html helpers 
+ * (both generate html tags based on code, tag helpers seem newer, sleeker).
+ */
+namespace WidgetWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
